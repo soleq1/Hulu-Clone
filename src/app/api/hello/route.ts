da22b265ee9f633d6379ddac573b6312c292caa0
Eddie Zieleniewski <solesskidder1@gmail.com>
2023-08-09 18:14:40 -0400

    Hulu-Clone fix

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,6 @@
+export async function NewsData(request: Request){
+    
+    // const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=f55b0c8c114c40f8aeb456ba90c3c0af')
+    // const data = await response.json()
+    // return new  Response(JSON.stringify(data));
+}
